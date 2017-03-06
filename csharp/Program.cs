c07e83dd72144c6af57b8f06d09998bba6ceeba1
Qinen Zhu <qinzhu@microsoft.com>
2017-03-06 18:26:59 +0800

    Add csharp part

---
+++ csharp/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Microsoft.DocAsCode.DataContracts.Common;
+using Microsoft.DocAsCode.Common;
+using YamlDotNet.Core;
+
+namespace SplitToc
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine($"Usage: {AppDomain.CurrentDomain.FriendlyName} [toc_path]");
+                return 1;
+            }
+
+            var tocPath = args[0];
+            SplitToc(tocPath);
+            return 0;
+        }
+
+        private static void SplitToc(string originalTocPath)
+        {
+            if (!Path.IsPathRooted(originalTocPath))
+            {
+                originalTocPath = Path.GetFullPath(originalTocPath);
+            }
+
+            if (!File.Exists(originalTocPath))
+            {
+                throw new FileNotFoundException($"The path of toc file: {originalTocPath} can't be found");
+            }
+
+            var originalTocDir = Path.GetDirectoryName(originalTocPath);
+            if (originalTocDir == null)
+            {
+                throw new Exception($"The directory of {originalTocPath} can't be null");
+            }
+
+            TocViewModel tocModel;
+            using (var stream = File.OpenRead(originalTocPath))
+            using (var reader = new StreamReader(stream))
+            {
+                try
+                {
+                    tocModel = YamlUtility.Deserialize<TocViewModel>(reader);
+                }
+                catch (YamlException ex)
+                {
+                    Console.WriteLine($"Error occurs while parsing toc {originalTocPath}, please check if the format is correct. {ex.Message}");
+                    throw;
+                }
+            }
+
+            if (tocModel == null)
+            {
+                Console.WriteLine($"No toc model parsed for {originalTocPath}.");
+                return;
+            }
+
+            var mergedTocModel = new TocViewModel();
+            foreach (var ns in tocModel)
+            {
+                var splittedTocPath = Path.Combine(originalTocDir, "_splitted", ns.Uid, "toc.yml");
+                var splittedTocRelativePath = PathUtility.MakeRelativePath(originalTocDir, splittedTocPath);
+
+                var splittedTocDir = Path.GetDirectoryName(splittedTocPath);
+                if (splittedTocDir == null)
+                {
+                    throw new Exception($"The directory of {splittedTocPath} can't be null");
+                }
+
+                if (!Directory.Exists(splittedTocDir))
+                {
+                    Directory.CreateDirectory(splittedTocDir);
+                }
+
+                var splittedTocItem = new TocItemViewModel
+                {
+                    Uid = ns.Uid,
+                    Name = ns.Name,
+                    Items = ns.Items
+                };
+                var splittedTocModel = new TocViewModel(new List<TocItemViewModel> {splittedTocItem});
+
+                YamlUtility.Serialize(splittedTocPath, splittedTocModel);
+                Console.WriteLine($"Create new splitted toc ({splittedTocPath})");
+
+                var mergedTocItem = new TocItemViewModel
+                {
+                    Uid = ns.Uid,
+                    Name = ns.Name,
+                    Href = Path.GetDirectoryName(splittedTocRelativePath).Replace('\\', '/') + "/"
+                };
+
+                mergedTocModel.Add(mergedTocItem);
+            }
+
+            YamlUtility.Serialize(originalTocPath, mergedTocModel);
+            Console.WriteLine($"Rewrite original toc file ({originalTocPath})");
+        }
+    }
+}
---
+++ csharp/tools/run.ps1
@@ -0,0 +1,26 @@
+param (
+    [parameter(mandatory=$true)]
+    [hashtable]$ParameterDictionary
+)
+
+$errorActionPreference = 'Stop'
+
+$currentDir = $($MyInvocation.MyCommand.Definition) | Split-Path
+$repositoryRoot = $ParameterDictionary.environment.repositoryRoot
+$tocPath = $ParameterDictionary["toc_path_need_to_be_splitted"]
+if ($tocPath)
+{
+	Write-Host "toc_path_need_to_be_splitted in openpublish.config.json can't be found, skip to split toc.yml into namespace level.";
+	exit 1;
+}
+
+$tocPath = Join-Path $repositoryRoot $tocPath
+$splitTocExeName = "SplitToc.exe"
+$splitTocExePath = Join-Path $currentDir $splitTocExeName
+
+Write-Host "Executing $splitTocExePath $tocPath" | timestamp
+& "$splitTocExePath" $tocPath
+if ($LASTEXITCODE -ne 0)
+{
+    exit $LASTEXITCODE
+}
