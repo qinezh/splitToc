efbf91c10bc5560c1bc76a83e1d30e4c73a5d085
Qinen Zhu <qinzhu@microsoft.com>
2017-04-26 12:18:03 +0800

    Support to split multiple toc.yml

--- csharp/tools/run.ps1
+++ csharp/tools/run.ps1
@@ -1,5 +1,5 @@
 param (
-    [parameter(mandatory=$true)]
+    [parameter(mandatory = $true)]
     [hashtable]$ParameterDictionary
 )
 
@@ -7,20 +7,31 @@ $errorActionPreference = 'Stop'
 
 $currentDir = $($MyInvocation.MyCommand.Definition) | Split-Path
 $repositoryRoot = $ParameterDictionary.environment.repositoryRoot
-$tocPath = $ParameterDictionary.environment.publishConfigContent.toc_path_need_to_split
-if ([string]::IsNullOrEmpty($tocPath))
-{
-	Write-Host "toc_path_need_to_split in openpublish.config.json can't be found, skip to split toc.yml into namespace level.";
-	exit 1;
-}
-
-$tocPath = Join-Path $repositoryRoot $tocPath
 $splitTocExeName = "SplitToc.exe"
 $splitTocExePath = Join-Path $currentDir $splitTocExeName
+$tocPath = $ParameterDictionary.environment.publishConfigContent.toc_path_need_to_split
 
-Write-Host "Executing $splitTocExePath $tocPath" | timestamp
-& "$splitTocExePath" $tocPath
-if ($LASTEXITCODE -ne 0)
-{
-    exit $LASTEXITCODE
+if (!$tocPath) {
+    Write-Host "toc_path_need_to_split in openpublish.config.json can't be found, skip to split toc.yml into namespace level.";
+    exit 1;
+}
+
+function SplitToc {
+    param($tocPath)
+    Write-Host "Executing $splitTocExePath $tocPath" | timestamp
+    & "$splitTocExePath" $tocPath
+    if ($LASTEXITCODE -ne 0) {
+        exit $LASTEXITCODE
+    }
+}
+
+if ($tocPath -is [System.Array]) {
+    foreach ($singlePath in $tocPath) {
+        $singlePath = Join-Path $repositoryRoot $singlePath
+        SplitToc($singlePath)
+    }
+}
+else {
+    $tocPath = Join-Path $repositoryRoot $tocPath
+    SplitToc($tocPath)
 }
